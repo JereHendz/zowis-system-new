f9f3568878831c340fc2e04797dc06b98d8c98d6
JereHendz <jereomarr@gmal.com>
2022-04-25 20:25:01 -0600

    Set up the url

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,10 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost:8080/';
+    // public $baseURL = 'http://localhost/zowis-system-new';
+    public $baseURL = 'https://ec2-18-117-137-48.us-east-2.compute.amazonaws.com/zowis-system-new/';
+
+
 
     /**
      * --------------------------------------------------------------------------
