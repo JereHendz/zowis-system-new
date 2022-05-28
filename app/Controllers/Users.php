9a8b2a78afe9afd59453a20543b85e09ac31b6b0
ElmerG <elmguz74@hotmail.com>
2022-05-28 11:55:38 -0600

    edit user

--- app/Controllers/Users.php
+++ app/Controllers/Users.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use CodeIgniter\RESTful\ResourceController;
 use App\Models\UserModel;
 use App\Models\EmployeeModel;
+use App\Models\ProcessStateModel;
 
 date_default_timezone_set('America/El_Salvador');
 
@@ -16,10 +17,23 @@ class Users extends ResourceController
         //Get all users
         $userModel = new UserModel();
         $employeeModel = new EmployeeModel();
+        $processState = new ProcessStateModel();
 
         $data = array(
             'users' => $userModel->findAll(),
             'employees' => $employeeModel->findAll(),
+            'status' => $processState->getStateByType(1)
+        );
+
+        return $this->respond($data);
+    }
+
+    public function show($id = null){
+        //Get all users
+        $userModel = new UserModel();
+
+        $data = array(
+            'user' => $userModel->find($id),
         );
 
         return $this->respond($data);
@@ -64,37 +78,39 @@ class Users extends ResourceController
     }
 
     public function update($id = null){
-
         $form=$this->request->getJSON(true);
+        $userModel = new UserModel();
+        
         if(empty($form)){
             return $this->failValidationErrors('Nothing to update');
         }
 
-        if(!$this->model->find($id)){
+        if(!$userModel->find($id)){
             return $this->failNotFound();
         }
 
-        if(!$this->model->update($id, $form)){
-            return $this->failValidationErrors($this->model->errors());
+        if(!$userModel->update($id, $form)){
+            return $this->failValidationErrors($userModel->errors());
         }
 
         return $this->respondUpdated([
             'message'=>'Updated successfully',
-            'data'=>$this->model->find($id),
+            'data'=>$userModel->find($id),
         ]);
     }
     
-    public function delete($id=null){
-        if (!$this->model->find($id)) {
-            return $this->failNotFound();
-        }
+    
+    // public function delete($id=null){
+    //     if (!$this->model->find($id)) {
+    //         return $this->failNotFound();
+    //     }
 
-        $this->model->where('id',$id)->delete();
+    //     $this->model->where('id',$id)->delete();
 
-        return $this->respondDeleted([
-            'message'=>"Registro {$id} fue eliminado"
-        ]);
-    }
+    //     return $this->respondDeleted([
+    //         'message'=>"Registro {$id} fue eliminado"
+    //     ]);
+    // }
 
     public function login(){
         $encrypter = \Config\Services::encrypter();
